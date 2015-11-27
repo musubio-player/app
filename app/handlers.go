8b4e908a544dab6dfa33374e31cbb15dead79f3f
khalib <caleb.strumms@gmail.com>
2015-11-27 09:18:17 -0800

    Modified request to YouTube not to use flags.

--- app/handlers.go
+++ app/handlers.go
@@ -7,32 +7,22 @@ import (
 	"io/ioutil"
 	"net/http"
 	//	"os"
-	"flag"
-	"log"
 	"time"
 
 	"code.google.com/p/google-api-go-client/googleapi/transport"
 	"code.google.com/p/google-api-go-client/youtube/v3"
+	log "github.com/Sirupsen/logrus"
 	"github.com/gorilla/mux"
 )
 
 const developerKey = "AIzaSyDfvJI8TlhqvWYEWSgyskBc2lKvGqPUUQk"
-
-var (
-	query      = flag.String("query", "sfgiants", "Search term")
-	maxResults = flag.Int64("max-results", 25, "Max YouTube results")
-)
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome!")
 }
 
 func SearchIndex(w http.ResponseWriter, r *http.Request) {
-	//	vars := mux.Vars(r)
-	//	q := vars["q"]
-
-	// YT Start
-	flag.Parse()
+	q := r.URL.Query().Get("q")
 
 	client := &http.Client{
 		Transport: &transport.APIKey{Key: developerKey},
@@ -45,9 +35,10 @@ func SearchIndex(w http.ResponseWriter, r *http.Request) {
 
 	// Make the API call to YouTube.
 	call := service.Search.List("id,snippet").
-		Q(*query).
-		MaxResults(*maxResults)
+		Q(q).
+		MaxResults(25)
 	response, err := call.Do()
+
 	if err != nil {
 		log.Fatalf("Error making search API call: %v", err)
 	}
@@ -97,11 +88,6 @@ func SearchIndex(w http.ResponseWriter, r *http.Request) {
 		SearchItems:   items,
 	}
 
-	//	printIDs("Videos", videos)
-	//	printIDs("Channels", channels)
-	//	printIDs("Playlists", playlists)
-	// END YT
-
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.Header().Set("Access-Control-Allow-Origin", "http://local.musubio.com")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
@@ -111,18 +97,6 @@ func SearchIndex(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewEncoder(w).Encode(result); err != nil {
 		panic(err)
 	}
-}
-
-// Print the ID and title of each result in a list as well as a name that
-// identifies the list. For example, print the word section name "Videos"
-// above a list of video search results, followed by the video ID and title
-// of each matching video.
-func printIDs(sectionName string, matches map[string]string) {
-	fmt.Printf("%v:\n", sectionName)
-	for id, title := range matches {
-		fmt.Printf("[%v] %v\n", id, title)
-	}
-	fmt.Printf("\n\n")
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
