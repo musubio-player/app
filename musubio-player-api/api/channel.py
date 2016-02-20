2ff178f7fce810cb590799f410a7fc2c05d35f18
khalib <caleb.strumms@gmail.com>
2016-02-20 08:34:39 -0800

    Added memcache layer on channel calls.

--- musubio-player-api/api/channel.py
+++ musubio-player-api/api/channel.py
@@ -3,10 +3,12 @@
 Defined here are the ProtoRPC messages needed to define Schemas for methods
 as well as those methods defined in an API.
 """
-import endpoints
+import endpoints, logging
 from protorpc import message_types
 from protorpc import remote
 from api import api_root
+
+from google.appengine.api import memcache
 
 from messages.channel import Channel, ChannelInsertRequest, ChannelListResponse, ChannelDetailsRequest, ChannelAddVideoRequest
 from models.channel import ChannelModel
@@ -23,7 +25,21 @@ class ChannelApi(remote.Service):
                       http_method='GET',
                       name='channels.details')
     def channel_details(self, request):
-        channel = ChannelModel.get_details(request)
+        logging.info('channel_details()')
+
+        cache_key = 'channels/details/%s' % request.id
+        channel = memcache.get(cache_key)
+        if channel is None:
+            channel = ChannelModel.get_details(request)
+
+            # Cache results
+            memcache.add(cache_key, channel)
+
+            logging.info('Channel details cached')
+        else:
+            logging.info('Channel details read from cache')
+
+
         return channel.to_message()
 
     @endpoints.method(message_types.VoidMessage,
@@ -32,8 +48,20 @@ class ChannelApi(remote.Service):
                       http_method='GET',
                       name='channels.list')
     def channel_list(self, request):
-        query = ChannelModel.query_channels()
-        channels = [entity.to_message() for entity in query.fetch()]
+        logging.info('channel_list()')
+
+        cache_key = 'channels/list'
+        channels = memcache.get(cache_key)
+        if channels is None:
+            query = ChannelModel.query_channels()
+            channels = [entity.to_message() for entity in query.fetch()]
+
+            # Cache results
+            memcache.add(cache_key, channels)
+
+            logging.info('Channel list cached')
+        else:
+            logging.info('Channel list read from cache')
 
         channelList = ChannelListResponse()
         channelList.channels = channels
